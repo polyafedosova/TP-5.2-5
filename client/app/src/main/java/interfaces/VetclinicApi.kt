3bfc698cab37ace4fdc6aab69111fa89d7bdca54
Arteom <92199229+GigaIQ@users.noreply.github.com>
2023-05-12 13:46:32 +0300

    Update VetclinicDto's

--- client/app/src/main/java/interfaces/VetclinicApi.kt
+++ client/app/src/main/java/interfaces/VetclinicApi.kt
@@ -1,18 +1,19 @@
 package interfaces
 
-import dto.VetclinicDto
+import dto.VetclinicDtoGet
+import dto.VetclinicDtoPost
 import retrofit2.Call
 import retrofit2.http.*
 
 interface VetclinicApi {
     @GET("/vetclinics")
-    fun getAllVetclinics(): Call<List<VetclinicDto>>
+    fun getAllVetclinics(): Call<List<VetclinicDtoGet>>
 
     @POST("/vetclinics/new")
-    fun saveNewVetclinic(@Body vetclinic: VetclinicDto): Call<Void>
+    fun saveNewVetclinic(@Body vetclinic: VetclinicDtoPost): Call<Void>
 
     @PUT("/vetclinics/{id}/update")
-    fun updateVetclinic(@Path("id") id: Int, @Body vetclinic: VetclinicDto): Call<Void>
+    fun updateVetclinic(@Path("id") id: Int, @Body vetclinic: VetclinicDtoGet): Call<Void>
 
     @DELETE("/vetclinics/{id}/delete")
     fun deleteVetclinic(@Path("id") id: Int): Call<Void>
