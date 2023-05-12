e90a43e1ed0eb8f18d1d3c712a050a368df3ee7a
Arteom <92199229+GigaIQ@users.noreply.github.com>
2023-05-12 13:47:40 +0300

    Update open clinic fregment

--- client/app/src/main/java/medical/SpecificFragment.kt
+++ client/app/src/main/java/medical/SpecificFragment.kt
@@ -9,6 +9,17 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import dto.TreatmentDto
+import dto.VetclinicDtoGet
+import interfaces.TreatmentApi
+import interfaces.VetclinicApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.vsu.cs.tp.paws.R
 
 class SpecificFragment : Fragment(){
@@ -18,25 +29,59 @@ class SpecificFragment : Fragment(){
     private lateinit var clinicAddress: TextView
     private lateinit var clinicServices: TextView
 
+    private var serviceList: List<TreatmentDto>? = null
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:8080")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view: View = inflater.inflate(R.layout.fragment_clinic_open, container, false)
 
         clinicName = view.findViewById(R.id.toolbar_name_text)
         clinicAddress = view.findViewById(R.id.clinicAddress)
-        clinicServices = view.findViewById(R.id.clinicServices)
+        clinicServices = view.findViewById(R.id.servicesTextView)
 
+        val id = requireArguments().getInt("id")
         val nameValue = requireArguments().getString("name")
-        val serviceValue = requireArguments().getString("service")
+//        val serviceValue = requireArguments().getString("service")
         val addressValue = requireArguments().getString("address")
+
+
 
         clinicName.text = nameValue
         clinicAddress.text = addressValue
-        clinicServices.text = serviceValue
+
+//        clinicServices.text = serviceValue
 
 //        Toast.makeText(this.requireContext(), nameValue, Toast.LENGTH_SHORT).show()
 //        Toast.makeText(this.requireContext(), serviceValue, Toast.LENGTH_SHORT).show()
 //        Toast.makeText(this.requireContext(), addressValue, Toast.LENGTH_SHORT).show()
+
+
+        val api = retrofit.create(TreatmentApi::class.java)
+
+//        try {
+//            CoroutineScope(Dispatchers.IO).launch {
+//
+//                requireActivity().runOnUiThread {
+//                    serviceList = api.getVetclinicTreatments(id).execute().body()
+//                }
+//            }
+//        } catch(ex: Exception) {
+//            ex.stackTrace
+//        }
+
+        var tempStr = ""
+
+        for (i in 0..9) {
+            tempStr += "name$i  price$i\n"
+        }
+
+
+        clinicServices.text = tempStr
 
         specificClinicBackButton = view.findViewById(R.id.specificClinicBackButton)
 
