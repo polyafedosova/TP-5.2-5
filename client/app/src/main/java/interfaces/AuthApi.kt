ddb2cab5a84b70b872937689b3dd7d9d0cd9e979
Arteom <92199229+GigaIQ@users.noreply.github.com>
2023-05-16 22:42:11 +0300

    Add autorisation function

---
+++ client/app/src/main/java/interfaces/AuthApi.kt
@@ -0,0 +1,19 @@
+package interfaces
+
+import dto.JwtGet
+import dto.JwtPost
+
+import retrofit2.Call
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface AuthApi {
+    @GET("api/auth/token")
+    fun getToken(): Response
+
+    @POST("api/auth/login")
+    fun login(@Body jwt: JwtPost): Call<JwtGet>
+
+}
