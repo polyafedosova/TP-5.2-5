3c5e274d07e66a7dc883b0f0a7e53b6a2ee18401
Arteom <92199229+GigaIQ@users.noreply.github.com>
2023-06-02 17:41:07 +0300

    Add onboard

---
+++ client/app/src/main/java/onboard/OnboardFragmentFirst.kt
@@ -0,0 +1,19 @@
+package onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.vsu.cs.tp.paws.R
+
+
+class OnboardFragmentFirst : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_onboard_first, container, false)
+    }
+
+}
---
+++ client/app/src/main/java/onboard/OnboardFragmentSecond.kt
@@ -0,0 +1,20 @@
+package onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.vsu.cs.tp.paws.R
+
+
+class OnboardFragmentSecond : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_onboard_second, container, false)
+    }
+
+
+}
