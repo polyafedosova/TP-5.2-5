198963aaf46208d3eafc0bafd74c3c7eeb1768f4
Ryneedom <121516680+Ryneedom@users.noreply.github.com>
2023-06-04 13:28:20 +0300

    update getNyOwnerTest dogservice

--- dog-app/server/src/test/java/ru/vsu/dogapp/service/DogServiceTest.java
+++ dog-app/server/src/test/java/ru/vsu/dogapp/service/DogServiceTest.java
@@ -18,6 +18,7 @@ import ru.vsu.dogapp.repository.OwnerRepository;
 import javax.transaction.Transactional;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -65,7 +66,7 @@ class DogServiceTest extends IntegrationEnvironment {
     void updateTest() {
         //given
         DogDto oldDogDto = new DogDto(1,"Artem","2002-06-02",true,"FKn");
-        DogDto newDogDto = new DogDto(2,"Kirill","2002-06-02",true,"FKn");
+        DogDto newDogDto = new DogDto(2,"Kirill","2003-05-02",true,"FKn");
         Owner owner = new Owner(2,"Posix", "Abcd123*@", "Polina", true, Collections.singleton(Role.USER));
         ownerRepository.save(owner);
         service.save(owner.getUsername(), oldDogDto);
@@ -90,7 +91,7 @@ class DogServiceTest extends IntegrationEnvironment {
     void deleteTest() {
         //given
         DogDto dogDto = new DogDto(1,"Artem","2002-06-02",true,"FKn");
-        DogDto newDogDto = new DogDto(2,"Kirill","2002-06-02",true,"FKn");
+        DogDto newDogDto = new DogDto(2,"Kirill","2003-04-02",true,"FKN");
         Owner owner = new Owner(2,"Posix", "Abcd123*@", "Polina", true, Collections.singleton(Role.USER));
         ownerRepository.save(owner);
 
@@ -115,7 +116,31 @@ class DogServiceTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void getByOwner() {
-        
+    void getByOwnerTest() {
+        DogDto dogDto1 = new DogDto(1,"Artem","2002-06-02",true,"FKn");
+        DogDto dogDto2 = new DogDto(2,"Kirill","2001-05-10",true,"FKn");
+        DogDto dogDto3 = new DogDto(3,"Roman","2012-03-01",true,"FKn");
+        Owner owner1 = new Owner(4,"Posix", "Abcd123*@", "Polina", true, Collections.singleton(Role.USER));
+        ownerRepository.save(owner1);
+        Owner owner2 = new Owner(5,"Makson", "Abcd123*@", "Maks", true, Collections.singleton(Role.USER));
+        ownerRepository.save(owner2);
+
+        service.save(owner1.getUsername(), dogDto1);
+        service.save(owner1.getUsername(), dogDto2);
+        service.save(owner2.getUsername(), dogDto3);
+        // when
+        List<DogDto> list = service.getByOwner(owner1.getUsername());
+        // then
+        assertThat(list.get(0), is(notNullValue()));
+        assertThat(list.get(0).getName(), is(equalTo(dogDto1.getName())));
+        assertThat(list.get(0).getBreed(), is(equalTo(dogDto1.getBreed())));
+        assertThat(list.get(0).getBirthday(), is(equalTo(dogDto1.getBirthday())));
+        assertThat(list.get(0).getSex(), is(equalTo(dogDto1.getSex())));
+        assertThat(list.get(1), is(notNullValue()));
+        assertThat(list.get(1).getName(), is(equalTo(dogDto2.getName())));
+        assertThat(list.get(1).getBreed(), is(equalTo(dogDto2.getBreed())));
+        assertThat(list.get(1).getBirthday(), is(equalTo(dogDto2.getBirthday())));
+        assertThat(list.get(1).getSex(), is(equalTo(dogDto2.getSex())));
     }
+
 }
