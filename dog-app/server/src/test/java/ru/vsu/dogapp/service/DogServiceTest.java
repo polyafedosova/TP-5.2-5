75df82d772b70397d8a711b00b8a58c713370d65
Ryneedom <121516680+Ryneedom@users.noreply.github.com>
2023-06-04 12:33:51 +0300

    add updateTest

--- dog-app/server/src/test/java/ru/vsu/dogapp/service/DogServiceTest.java
+++ dog-app/server/src/test/java/ru/vsu/dogapp/service/DogServiceTest.java
@@ -9,8 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.vsu.dogapp.IntegrationEnvironment;
 import ru.vsu.dogapp.dto.DogDto;
-import ru.vsu.dogapp.dto.OwnerDto;
-import ru.vsu.dogapp.entity.Dog;
 import ru.vsu.dogapp.entity.Owner;
 import ru.vsu.dogapp.entity.type.Role;
 import ru.vsu.dogapp.mapper.DogMapper;
@@ -25,7 +23,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -43,7 +40,7 @@ class DogServiceTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void save() {
+    void saveTest() {
         //given
         DogDto dogDto = new DogDto(1,"Artem","2002-06-02",true,"FKn");
         Owner owner = new Owner(2,"Posix", "Abcd123*@", "Polina", true, Collections.singleton(Role.USER));
@@ -64,7 +61,25 @@ class DogServiceTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void update() {
+    void updateTest() {
+        //given
+        DogDto oldDogDto = new DogDto(1,"Artem","2002-06-02",true,"FKn");
+        DogDto newDogDto = new DogDto(2,"Kirill","2002-06-02",true,"FKn");
+        Owner owner = new Owner(2,"Posix", "Abcd123*@", "Polina", true, Collections.singleton(Role.USER));
+        ownerRepository.save(owner);
+        service.save("Posix", oldDogDto);
+        service.update(1,newDogDto);
+
+        //when
+        var response = repository.findDogById(1);
+
+        //then
+        assertThat(response, is(notNullValue()));
+        assertThat(response.getName(), is(equalTo(newDogDto.getName())));
+        assertThat(response.getBreed(), is(equalTo(newDogDto.getBreed())));
+        assertThat(response.getBirthday(), is(equalTo(newDogDto.getBirthday())));
+        assertThat(response.getSex(), is(equalTo(newDogDto.getSex())));
+        assertThat(response.getOwner().getName(), is(equalTo(owner.getName())));
     }
 
 
